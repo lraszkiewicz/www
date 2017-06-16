4ddba3500fbdca26576884b8ff7a43bf45adb48b
Łukasz Raszkiewicz <lraszkiewicz@gmail.com>
2017-06-16 15:11:34 +0200

    Task 4

--- task4/elections/admin.py
+++ task4/elections/admin.py
@@ -2,10 +2,22 @@ from django.contrib import admin
 
 from .models import *
 
+
+class ResultsAdmin(admin.ModelAdmin):
+    fields = ['eligible_voters', 'issued_ballots', 'spoilt_ballots']
+
+
+class PlaceAdmin(admin.ModelAdmin):
+    search_fields = ['address', 'number', 'voivodeship__name', 'district__id', 'municipality__name', 'municipality__id']
+    list_display = ['number', 'address', 'voivodeship', 'district', 'municipality']
+    fields = ['number', 'address', 'voivodeship', 'district', 'municipality']
+
+
 admin.site.register(Candidate)
 admin.site.register(Voivodeship)
 admin.site.register(District)
 admin.site.register(Municipality)
-admin.site.register(Place)
+admin.site.register(Place, PlaceAdmin)
 admin.site.register(Votes)
 admin.site.register(ProtocolFile)
+admin.site.register(Results, ResultsAdmin)
--- task4/elections/models.py
+++ task4/elections/models.py
@@ -2,7 +2,9 @@ import os
 
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import Sum
 
 
 class Candidate(models.Model):
@@ -54,14 +56,40 @@ class Place(models.Model):  # obwód
 
 
 class Results(models.Model):  # wyniki w danym miejscu
-    voivodeship = models.ForeignKey('Voivodeship', null=True)
-    district = models.ForeignKey('District', null=True)
-    municipality = models.ForeignKey('Municipality', null=True)
-    place = models.ForeignKey('Place', null=True)
+    voivodeship = models.OneToOneField('Voivodeship', null=True, blank=True)
+    district = models.OneToOneField('District', null=True, blank=True)
+    municipality = models.OneToOneField('Municipality', null=True, blank=True)
+    place = models.OneToOneField('Place', null=True, blank=True)
     # exactly one of the above should be not null
     eligible_voters = models.IntegerField()  # uprawnieni
     issued_ballots = models.IntegerField()  # wydane karty
     spoilt_ballots = models.IntegerField()  # głosy nieważne
+
+    def save(self, *args, **kwargs):
+        c = (self.voivodeship is not None) + (self.district is not None)\
+            + (self.municipality is not None) + (self.place is not None)
+        if c != 1:
+            raise ValidationError('Exactly one of: voivodeship, district, municipality, place has to be not None.')
+        if self.place:
+            self.municipality.results.update_results()
+            self.district.results.update_results()
+            self.voivodeship.results.update_results()
+        super(Results, self).save(self, *args, **kwargs)
+
+    def update_results(self):
+        place_qs = None
+        if self.voivodeship:
+            place_qs = Place.objects.filter(voivodeship=self.voivodeship)
+        elif self.district:
+            place_qs = Place.objects.fitler(district=self.district)
+        elif self.municipality:
+            place_qs = Place.objects.filter(municipality=self.municipality)
+        if place_qs:
+            agg = place_qs.aggregate(eli=Sum('eligible_voters'), iss=Sum('issued_ballots'), spo=Sum('spoilt_ballots'))
+            self.eligible_voters = agg['eli'] or 0
+            self.issued_ballots = agg['iss'] or 0
+            self.spoilt_ballots = agg['spo'] or 0
+            self.save()
 
     def parent_str(self):
         if self.voivodeship:
@@ -77,6 +105,9 @@ class Results(models.Model):  # wyniki w danym miejscu
         return '{} - {}, {}, {}'.format(self.parent_str(), self.eligible_voters,
                                         self.issued_ballots, self.spoilt_ballots)
 
+    class Meta:
+        verbose_name_plural = "results"
+
 
 class Votes(models.Model):
     amount = models.IntegerField()
@@ -89,6 +120,31 @@ class Votes(models.Model):
     class Meta:
         unique_together = ('candidate', 'parent')
         verbose_name_plural = "votes"
+
+    def save(self, *args, **kwargs):
+        super(Votes, self).save(*args, **kwargs)
+        if self.parent.place is not None:
+            c = self.candidate
+            p = self.parent.place
+            Votes.objects.get(parent__municipality=p.municipality, candidate=c).update_votes()
+            Votes.objects.get(parent__district=p.district, candidate=c).update_votes()
+            Votes.objects.get(parent__voivodeship=p.voivodeship, candidate=c).update_votes()
+
+    def update_votes(self):
+        c = self.candidate
+        p = self.parent
+        v = None
+
+        if p.municipality:
+            v = Votes.objects.filter(parent__place__municipality=p.municipality, candidate=c)
+        elif p.district:
+            v = Votes.objects.filter(parent__place__district=p.district, candidate=c)
+        elif p.voivodeship:
+            v = Votes.objects.filter(parent__place__voivodeship=p.voivodeship, candidate=c)
+
+        if v:
+            self.amount = v.aggregate(Sum('amount'))['amount__sum'] or 0
+            self.save()
 
 
 def protocol_file_path(instance, filename):
